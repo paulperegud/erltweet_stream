e03de45ac566cca8ca0c6603cf659c1a86582595
Paul Peter Flis <pawel@flycode.pl>
2013-07-31 21:26:42 +0200

    nice print logs

--- include/erltweet_stream.hrl
+++ include/erltweet_stream.hrl
@@ -11,8 +11,7 @@
 
 -define(URI_FILTER, "https://stream.twitter.com/1.1/statuses/filter.json").
 
--define(WARN_LOG(X, Y), (error_logger:warning_msg(X, Y)) ).
--define(ERR_LOG(X, Y), (error_logger:error_msg(X, Y)) ).
-
-                
-
+-define(LOG(X), (lists:flatten(io_lib:format("=NODE: ~p | MODULE: ~p | PID: ~p | LINE: ~p ===~n~s", [node(), ?MODULE, self(), ?LINE, X]))) ).
+-define(ERR_LOG(X, Y), (error_logger:error_msg(?LOG(X), Y)) ).
+-define(WARN_LOG(X, Y), (error_logger:warning_msg(?LOG(X), Y)) ).
+-define(INFO_LOG(X, Y), (error_logger:info_msg(?LOG(X), Y)) ).
